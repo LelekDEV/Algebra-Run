5599fefa7fbe5b474dd89298560c49af5839d6ed
Lelek <106480210+LelekDEV@users.noreply.github.com>
2024-02-01 18:49:31 +0100

    Update game files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 
 pygame.init()
 
@@ -9,8 +10,21 @@ clock = pygame.time.Clock()
 
 clicked = False
 
+# Here are some adjustable values
+PLAYER_SPEED = 16
+PLAYER_JUMP_HEIGHT = -36
+PLAYER_ORB_JUMP_HEIGHT = -32
+PLAYER_GRAVITY = 2.2
+PLAYER_ROTATION_SPEED = -6
+PLAYER_SNAP_LIMIT = 32
+
 def load_level(name):
 	data = []
+
+	if not os.path.isfile(f'{name}.lvl'):
+		file = open(f'{name}.lvl', 'a')
+		file.write('-\n-\n-\n-\n')
+		file.close()
 
 	with open(f'{name}.lvl', 'r') as file:
 		for line in file:
@@ -119,10 +133,17 @@ class Group(pygame.sprite.Group):
 			for rect in get_block_rects():
 				pygame.draw.rect(screen, [0, 0, 255], (rect.topleft - self.pos, rect.size), 4)
 
+			for pos in blocks:
+				if not (pos[0], pos[1] + 1) in blocks:
+					pygame.draw.rect(screen, [255, 255, 0], (pos[0] * 128 - self.pos.x, screen.get_height() - (pos[1] + 1) * 128 - self.pos.y, 128, PLAYER_SNAP_LIMIT), 4)
+
 			for rect in get_spike_rects():
 				new_rect = rect.copy()
 				new_rect.center -= self.pos
 				pygame.draw.rect(screen, [255, 0, 0], (rect.topleft - self.pos, rect.size), 4)
+
+			for rect in get_orb_rects():
+				pygame.draw.rect(screen, [0, 255, 0], (rect.topleft - self.pos, rect.size), 4)
 
 			pygame.draw.rect(screen, [255, 0, 0], (player.rect.topleft - self.pos, player.rect.size), 4)
 
@@ -143,69 +164,85 @@ class Player(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect(center = self.pos)
 		self.vel = pygame.math.Vector2()
 
-		self.rotation = 5
+		self.rotation = 0
 
 		self.grounded = False
+		self.from_ground = False
+		self.buffer = False
 
-		self.vel.x = 16
+		self.vel.x = PLAYER_SPEED
 
 		self.trail = []
 
 	def update(self):
+		if pygame.key.get_pressed()[pygame.K_UP]:
+			if not self.from_ground:
+				self.buffer = True
+		else:
+			self.buffer = False
+			self.from_ground = False
+
 		for rect in get_spike_rects():
 			if self.rect.colliderect(rect):
-				pass#self.kill()
+				if not noclip:
+					self.kill()
+					return
 
 		self.trail.append(self.pos.copy())
 
 		self.pos.x += self.vel.x
 		self.rect.centerx = self.pos.x
 
-		self.vel.y += 2.2
+		for rect in get_block_rects():
+			if self.rect.colliderect(rect):
+				self.rect.y -= PLAYER_SNAP_LIMIT
+				if self.rect.colliderect(rect):
+					self.rect.y += PLAYER_SNAP_LIMIT
+					if not noclip:
+						self.kill()
+						return
+				else:
+					self.snap_to_block(rect.top)
+
+		self.vel.y += PLAYER_GRAVITY
 
 		self.pos.y += self.vel.y
 		self.rect.centery = self.pos.y
 
-		self.rect.y += 1
-
-		for rect in get_orb_rects():
-			if self.rect.colliderect(rect):
-				if pygame.key.get_pressed()[pygame.K_UP]:
-					self.vel.y = -32
+		for i, rect in enumerate(get_orb_rects()):
+			if self.rect.colliderect(rect) and not orbs_activated[i]:
+				if self.buffer:
+					self.vel.y = PLAYER_ORB_JUMP_HEIGHT
+					self.buffer = False
+					orbs_activated[i] = True
 
 		for rect in get_block_rects():
 			if self.rect.colliderect(rect):
-				self.vel.y = 0
-				self.rect.bottom = rect.top
-				self.pos.y = self.rect.centery
-
-				if not self.grounded:
-					self.rotation = (self.rotation + 45) // 90 * 90
-
-				if pygame.key.get_pressed()[pygame.K_UP]:
-					self.vel.y = -36
-
-				self.grounded = True
-
+				self.snap_to_block(rect.top)
 				return
 
 		if self.pos.y > screen.get_height() - 64:
-			self.vel.y = 0
-			self.rect.bottom = screen.get_height()
-			self.pos.y = self.rect.centery
-
-			if not self.grounded:
-				self.rotation = (self.rotation + 45) // 90 * 90
-
-			if pygame.key.get_pressed()[pygame.K_UP]:
-				self.vel.y = -36
-
-			self.grounded = True
-
+			self.snap_to_block(screen.get_height())
 		else:
-			self.rotation -= 6
+			self.rotation += PLAYER_ROTATION_SPEED
 
 			self.grounded = False
+
+	def snap_to_block(self, pos):
+		self.vel.y = 0
+		self.rect.bottom = pos
+		self.pos.y = self.rect.centery
+
+		self.from_ground = True
+		self.buffer = False
+
+		if not self.grounded:
+			self.rotation = (self.rotation + 45) // 90 * 90
+
+		if pygame.key.get_pressed()[pygame.K_UP]:
+			self.vel.y = PLAYER_JUMP_HEIGHT
+
+		self.grounded = True
 
 	def kill(self):
 		global mode
@@ -216,14 +253,20 @@ group = Group()
 player = Player(group)
 
 blocks, spikes, orbs, rotated_tiles = load_level('level')
+orbs_activated = [False for i in range(len(orbs))]
 
 mode = 'play'
 switch_mode_cooldown = False
 edit_mode = blocks
 
+# Here are some optional modes
 show_hitboxes = False
 switch_show_hitboxes_cooldown = False
 
+noclip = False
+switch_noclip_cooldown = False
+
+# Change the song path right here
 pygame.mixer.music.load('song.ogg')
 pygame.mixer.music.play()
 
@@ -248,12 +291,16 @@ while True:
 				player.pos.y = screen.get_height() - 64
 				player.rect.center = player.pos
 
-				player.vel.x = 16
+				player.vel.x = PLAYER_SPEED
 				player.vel.y = 0
 
 				player.rotation = 0
 
 				player.trail = []
+
+				group.pos.y = 0
+
+				orbs_activated = [False for i in range(len(orbs))]
 
 				pygame.mixer.music.play()
 
@@ -268,6 +315,14 @@ while True:
 		switch_show_hitboxes_cooldown = True
 	else:
 		switch_show_hitboxes_cooldown = False
+
+	if pygame.key.get_pressed()[pygame.K_e]:
+		if not switch_noclip_cooldown:
+			noclip = not noclip
+
+		switch_noclip_cooldown = True
+	else:
+		switch_noclip_cooldown = False
 
 	if mode == 'play':
 		player.update()
@@ -285,7 +340,7 @@ while True:
 		if keys[pygame.K_4]:
 			edit_mode = rotated_tiles
 
-		pos = (int((pygame.mouse.get_pos()[0] + group.pos.x) // 128), int((screen.get_height() - pygame.mouse.get_pos()[1]) // 128))
+		pos = (int((pygame.mouse.get_pos()[0] + group.pos.x) // 128), int((screen.get_height() - pygame.mouse.get_pos()[1] - group.pos.y) // 128))
 
 		if pygame.mouse.get_pressed()[0]:
 			if not pos in edit_mode:
@@ -305,14 +360,28 @@ while True:
 		if pygame.key.get_pressed()[pygame.K_RIGHT]:
 			group.pos.x += 10
 
+		if pygame.key.get_pressed()[pygame.K_UP]:
+			group.pos.y -= 10
+
+		if pygame.key.get_pressed()[pygame.K_DOWN]:
+			group.pos.y += 10
+
 	screen.fill([255, 255, 255])
 
 	if mode == 'edit':
 		for x in range(16):
 			pygame.draw.line(screen, [0, 0, 0], (x * 128 - group.pos.x % 128, 0), (x * 128 - group.pos.x % 128, screen.get_height()))
 		for y in range(9):
-			pygame.draw.line(screen, [0, 0, 0], (0, y * 128 + screen.get_height() % 128), (screen.get_width(), y * 128 + screen.get_height() % 128))
+			pygame.draw.line(screen, [0, 0, 0], (0, y * 128 + screen.get_height() % 128 - group.pos.y % 128), (screen.get_width(), y * 128 + screen.get_height() % 128 - group.pos.y % 128))
 
 	group.draw()
+
+	if show_hitboxes and not noclip:
+		pygame.draw.circle(screen, [0, 0, 255], (screen.get_width() - 60, 60), 30, 10)
+	elif not show_hitboxes and noclip:
+		pygame.draw.circle(screen, [255, 0, 0], (screen.get_width() - 60, 60), 30, 10)
+	elif show_hitboxes and noclip:
+		pygame.draw.circle(screen, [0, 0, 255], (screen.get_width() - 60, 60), 30, 10)
+		pygame.draw.circle(screen, [255, 0, 0], (screen.get_width() - 150, 60), 30, 10)
 
 	pygame.display.update()
